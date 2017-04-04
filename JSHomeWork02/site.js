626c8512731578e8814a56b4bdd2eafdd6331b32
Michael Torgovitsky <m.torgovitsky@gmail.com>
2017-04-04 13:13:37 +0300

    Add project files.

---
+++ JSHomeWork02/site.js
@@ -0,0 +1,33 @@
+﻿var colorArray = ["red", "green", "blue", "yellow", "purple", "darkorchid", "springgreen"];
+
+var currentIndex = 0;
+
+var intervalId;
+
+var myNeonSign;
+
+function startBG() {
+    intervalId = setInterval(startMyNeon, 500);
+}
+
+function startMyNeon() {
+    myNeonSign.style.backgroundColor = colorArray[currentIndex];
+    currentIndex++;
+    currentIndex %= colorArray.length;
+}
+
+var userColor;
+
+function promptColor() {
+    userColor = prompt("What color would You like to see?", "gold");
+    myNeonSign.style.backgroundColor = userColor;
+}
+
+function stopBG() {
+    clearInterval(intervalId);
+}
+
+//getting the elment after it's being rendered on HTML
+document.addEventListener("DOMContentLoaded", function myFunction () {
+    myNeonSign = document.getElementById("neonSign");
+});
